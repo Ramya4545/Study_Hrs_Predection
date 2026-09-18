bbab53271902c1444710c23c1c5b95cf6862e621
Ramya4545 <ramyapaila732@gmail.com>
2026-09-18 10:55:19 +0530

    Refactor app.py for model loading and input handling

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(
 # -----------------------------
 # Load Model
 # -----------------------------
-model = joblib.load("Study_Hrs_Result_prediction_model.pkl")
+model = joblib.load("Student_Std_Hrs.pkl")
 
 # -----------------------------
 # Title
